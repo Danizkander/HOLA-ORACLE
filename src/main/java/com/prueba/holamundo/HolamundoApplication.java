7b8054210d9ce0aa7b26261946b3082cf193d5ba
DannahtheVegas <diana.vega.mt@correo.usa.edu.co>
2021-10-07 21:37:38 -0500

    version dannah

---
+++ src/main/java/com/prueba/holamundo/HolamundoApplication.java
@@ -0,0 +1,14 @@
+package com.prueba.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoApplication.class, args);
+                System.out.println("hola mundo hugo");
+	}
+
+}
